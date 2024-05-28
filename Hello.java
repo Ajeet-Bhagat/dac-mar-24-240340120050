b31da51721531d1974512aa122aa2c909615072d
Ajeet-Bhagat <ajeetbhagat1719@gmail.com>
2024-05-28 12:21:49 +0530

    Your commit message for b2

--- Hello.java
+++ Hello.java
@@ -3,5 +3,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
     }
 }
