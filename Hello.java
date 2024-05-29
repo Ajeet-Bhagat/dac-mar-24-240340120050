425269d46e45eab15722f52be3eca0e0179fe724
Ajeet-Bhagat <ajeetbhagat1719@gmail.com>
2024-05-29 08:55:46 +0530

    HR commit

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Here is Ajeet");
+        System.out.println("Here is Himanshu");
         int a=10;
         int b=20;
         System.out.println(a+b);
